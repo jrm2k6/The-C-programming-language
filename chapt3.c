e88ec32d302835110ce38a021387cdd332e04e10
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2012-07-23 23:31:06 +0200

    Exercise from chapt 3 - in progress

--- chapt3.c
+++ chapt3.c
@@ -5,6 +5,7 @@
 
 int binsearch_modified(int x, int v[], int n);
 void escape(char t[]);
+void expand(char s1[]);
 
 int main(int argc, char* argv[])
 {
@@ -12,9 +13,13 @@ int main(int argc, char* argv[])
   // int atest[] = {1,2,4,5,6,8,9};
   // printf("%d\n", binsearch_modified(6, atest, 7));
 
-  printf("------_Exercise 3.2 ---\n");
-  printf("------ escape(s) ---\n");
-  escape("bonjour les \n   amis  \tca va");
+  // printf("------_Exercise 3.2 ---\n");
+  // printf("------ escape(s) ---\n");
+  // escape("bonjour les \n   amis  \tca va");
+
+  printf("---- Exercise 3.3 ---\n");
+  printf("expand(s1,s2)\n");
+  expand("chA-Z9ma-xen");
   
  return 0; 
 }
@@ -61,6 +66,40 @@ void escape(char t[])
     i++;
     result[j] = '\0';
   }
+  printf("%s\n", result);
+}
+
+void expand(char s1[])
+{
+  char result[1000];
+  char f,l;
+  int i, j, ir;
+  
+  ir = 0;
+  for(i=0; i< strlen(s1); i++)
+  {
+    if(s1[i] == '-' && (i == 0 || i == strlen(s1))) 
+    {
+      result[ir++] = s1[i];
+    }
+    else if (s1[i] == '-')
+    {
+      f = s1[i-1];
+      l = s1[i+1];
+      if(f < l)
+      {
+        for(j = 1; j < l - f; j++)
+        {
+          result[ir++] = f + j;
+        }
+      }
+    }
+    else
+    {
+      result[ir++] = s1[i];
+    }
+  }
+  result[ir++] = '\0';
   printf("%s\n", result);
 }
 
