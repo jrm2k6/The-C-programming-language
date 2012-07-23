294f4a81f65dd5ae92f9f1dbb3527d3ce9a5f060
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2012-07-23 09:21:28 +0200

    Exercices from chapter 2 (3 missing)

--- chapt2.c
+++ chapt2.c
@@ -5,13 +5,33 @@
 
 void htoi(char s[]);
 int hl_to_int(char c);
+void squeeze(char s1[], char s2[]);
+int any(char s1[], char s2[]);
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+unsigned invert(unsigned x, int p, int n);
+unsigned rightrot(unsigned x, int n);
+void lower(char w[]);
+
 
 int main(int argc, char* argv[])
 {
-  printf("------_Exercise 2.3 ---\n");
-  printf("-- htoi --\n");
-  htoi("0X2b6");
-  htoi("0x2b6F56");
+  // printf("------_Exercise 2.3 ---\n");
+  // printf("-- htoi --\n");
+  // htoi("0X2b6");
+  // htoi("0x2b6F56");
+
+  // printf("------_Exercise 2.4 ---\n");
+  // printf("-- squeeze(s1,s2) --\n");
+  // squeeze("chien aquatoque", "velo");
+
+  // printf("------_Exercise 2.5 ---\n");
+  // printf("-- any(s1,s2) --\n");
+  // int position = any("chien aquatoque", "velo");
+  // printf("first char occurs at position : %d\n", position);
+
+  printf("------_Exercise 2.10 ---\n");
+  printf("-- lower --\n");
+  lower("Ceci Est UN TesT");
   
  return 0; 
 }
@@ -72,3 +92,76 @@ int hl_to_int(char c)
  return result;
   
 }
+
+
+
+
+void squeeze(char s1[], char s2[])
+{
+  char result[1000];
+  int i,j,k,in_s2 = 1;
+
+  k = 0;
+  for(i=0; i< strlen(s1); i++)
+  {
+    if(in_s2 == 0) in_s2 = 1;
+    for(j=0; j< strlen(s2); j++)
+    {
+      if(s1[i] != ' ' && s1[i] == s2[j]) in_s2 = 0;
+    }
+
+    if(in_s2 == 1) 
+    {
+      result[k] = s1[i];
+      k++;
+    }
+  }
+  result[k] = '\0';
+  printf("s1 - s2 : %s\n", result);
+}
+
+int any(char s1[], char s2[])
+{
+  char result[1000];
+  int i,j = 1;
+
+  for(i=0; i< strlen(s1); i++)
+  {
+    for(j=0; j< strlen(s2); j++)
+    {
+      if(s1[i] != ' ' && s1[i] == s2[j]) return i;
+    }
+  }
+  return -1;
+}
+
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+
+}
+
+unsigned invert(unsigned x, int p, int n)
+{
+
+}
+
+unsigned rightrot(unsigned x, int n)
+{
+
+}
+
+void lower(char w[])
+{
+  char result[100];
+  char nc;
+  int i;
+  for(i=0; i < strlen(w); i++)
+  {
+    nc = (w[i] >= 'A' && w[i] <= 'Z')? w[i] - 'A' + 'a' : w[i];
+    result[i] = nc;
+  }
+  printf("%s\n", result);
+}
+
+
+
